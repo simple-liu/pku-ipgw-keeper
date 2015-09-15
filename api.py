273fc56595df9660a8e7526f6fc391a0f961f989
Lei Yang <yltt1234512@gmail.com>
2015-09-15 23:34:43 +0800

    completed api

--- api.py
+++ api.py
@@ -1,7 +1,28 @@
-import json
 import requests
 
-# https://its.pku.edu.cn:5428/ipgatewayofpku?uid=$username&password=$password&range=$range&operation=$action&timeout=1
+
+class IPGWError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class Connectivity:
+    def __init__(self, domestic="http://www.baidu.com", international="http://www.example.com"):
+        self.domestic = domestic
+        self.international = international
+
+    def check_domestic(self):
+        try:
+            r = requests.get(self.domestic)
+            if r.status_code == 200:
+                return True
+            else:
+                return False
+        except:
+            return False
 
 
 class Account:
@@ -19,6 +40,12 @@ class Account:
                    'operation': 'connect',
                    'timeout': 1}
         r = requests.get("https://its.pku.edu.cn:5428/ipgatewayofpku", params=payload)
+        return_text = r.text
+        if return_text.find('SUCCESS=NO'):
+            if return_text.find('不在申请访问服务的范围内'):
+                raise IPGWError('calling API outside campus LAN')
+            if return_text.find('账户名错'):
+                raise IPGWError('authentication failed')
 
     def disconnect(self, disconnect_all=False):
         operation = "disconnect"
@@ -30,3 +57,9 @@ class Account:
                    'operation': operation,
                    'timeout': 1}
         r = requests.get("https://its.pku.edu.cn:5428/ipgatewayofpku", params=payload)
+        return_text = r.text
+        if return_text.find('SUCCESS=NO'):
+            if return_text.find('不在申请访问服务的范围内'):
+                raise IPGWError('calling API outside campus LAN')
+            if return_text.find('账户名错'):
+                raise IPGWError('authentication failed')
